80f5e736216e82eb49150ee0b3dca7863d0e79ac
Mahmoud Abdalmonam Abdelglil Kolib <200018343@must.edu.eg>
2025-04-07 20:16:03 +0200

    Add Category Repository

---
+++ eCommerce.Domain/Interfaces/CategorySpecific/ICategory.cs
@@ -0,0 +1,9 @@
+﻿using eCommerce.Domain.Entities;
+
+namespace eCommerce.Domain.Interfaces.CategorySpecific
+{
+    public interface ICategory
+    {
+        Task<IEnumerable<Product>> GetProductsByCategory(Guid categoryId);
+    }
+}
---
+++ eCommerce.Infrastructure/Repositories/CategorySpecific/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using eCommerce.Domain.Entities;
+using eCommerce.Domain.Interfaces.CategorySpecific;
+using eCommerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Infrastructure.Repositories.CategorySpecific
+{
+    public class CategoryRepository(AppDbContext context) : ICategory
+    {
+        public async Task<IEnumerable<Product>> GetProductsByCategory(Guid categoryId)
+        {
+           var products = await context.Products
+                .Include(x=>x.Category)
+                .Where(x=>x.CategoryId == categoryId).AsNoTracking().ToListAsync();
+            return products.Count > 0 ? products : [];
+        }
+    }
+}
