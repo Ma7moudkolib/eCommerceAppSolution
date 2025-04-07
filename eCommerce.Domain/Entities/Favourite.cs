ddb7bc7e4d019197662a8e52e65753ead7deb5e0
Mahmoud Abdalmonam Abdelglil Kolib <200018343@must.edu.eg>
2025-04-07 20:12:09 +0200

    add Favorite Entity

---
+++ eCommerce.Domain/Entities/Favourite.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Domain.Entities
+{
+    public class Favourite
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
+        public Guid ProductId { get; set; }
+        public string? UserId { get; set; }
+        public DateTime CreatedData { get; set; } = DateTime.Now;
+        Product? Product { get; set; }
+
+    }
+}
