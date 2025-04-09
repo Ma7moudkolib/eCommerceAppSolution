3dcb53dcad528857765714bc04a8367a85cb95f2
Mahmoud Abdalmonam Abdelglil Kolib <200018343@must.edu.eg>
2025-04-09 11:18:11 +0200

    deleted any Referance not useed

--- eCommerce.Infrastructure/Middleware/ExceptionHandlingMiddleware.cs
+++ eCommerce.Infrastructure/Middleware/ExceptionHandlingMiddleware.cs
@@ -3,12 +3,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace eCommerce.Infrastructure.Middleware
 {
     public class ExceptionHandlingMiddleware(RequestDelegate _next )
