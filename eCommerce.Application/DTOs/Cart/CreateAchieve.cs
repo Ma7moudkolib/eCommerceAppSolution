af28797572fb119d9cec1d394e7ccb5e90935c23
Mahmoud Abdalmonam Abdelglil Kolib <200018343@must.edu.eg>
2025-04-09 11:08:06 +0200

    update type of user id form Guid to string

--- eCommerce.Application/DTOs/Cart/CreateAchieve.cs
+++ eCommerce.Application/DTOs/Cart/CreateAchieve.cs
@@ -1,10 +1,14 @@
-﻿namespace eCommerce.Application.DTOs.Cart
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Application.DTOs.Cart
 {
     public class CreateAchieve
     {
-        
+        [Required]
         public required Guid ProductId { get; set; }
+        [Required]
         public required int Quantity { get; set; }
-        public required Guid UserId { get; set; }
+        [Required]
+        public  string? UserId { get; set; }
     }
 }
