74869bf479cfbefea438e98e237928dd43f66b0f
Mahmoud Kolib <mahmoudkolib22@gmial.com>
2025-11-27 17:08:15 +0200

    add custom product repository

---
+++ eCommerce.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using eCommerce.Domain.Entities;
+
+namespace eCommerce.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(Product product);
+        Task<Product?> GetProductByIdAsync(int productId,bool trackChanges);
+        Task<IEnumerable<Product>> GetAllProductsAsync(bool trackChanges);
+        Task<IEnumerable<Product>> GetProductsByCategory(int categoryId, bool trackChanges);
+    }
+}
---
+++ eCommerce.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using eCommerce.Domain.Entities;
+using eCommerce.Domain.Interfaces;
+using eCommerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Infrastructure.Repositories
+{
+    public sealed class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext context) : base(context) { }
+        public void AddProduct(Product product) => Create(product);
+
+        public void DeleteProduct(Product product) => Delete(product);
+
+        public void UpdateProduct(Product product) => Update(product);
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync(bool trackChanges) =>
+            await FindAll(trackChanges).OrderBy(p=>p.Name).ToListAsync();
+
+        public async Task<Product?> GetProductByIdAsync(int productId, bool trackChanges) =>
+            await FindByCondition(p => p.Id == productId, trackChanges)
+            .SingleOrDefaultAsync();
+
+        public async Task<IEnumerable<Product>> GetProductsByCategory(int categoryId, bool trackChanges) =>
+            await FindByCondition(p => p.CategoryId == categoryId, trackChanges)
+            .ToListAsync();
+
+    }
+}
