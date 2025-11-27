c7a465dd15876b1aaad1cb7549d1667b0ac0d182
Mahmoud Kolib <mahmoudkolib22@gmial.com>
2025-11-27 17:19:22 +0200

    rename unit of work class to repository manager

---
+++ eCommerce.Domain/Interfaces/UnitOfWork/IRepositoryManager.cs
@@ -0,0 +1,15 @@
+﻿using eCommerce.Domain.Entities;
+using eCommerce.Domain.Interfaces.Authentication;
+using eCommerce.Domain.Interfaces.Cart;
+namespace eCommerce.Domain.Interfaces.UnitOfWork
+{
+    public interface IRepositoryManager
+    {
+        IProductRepository Product { get; }
+        ICategory Categorie { get; }
+        IFavourite Favorite { get; }
+        ICart Cart { get; }
+        IPaymentMethod PaymentMethod { get; }
+        Task<int> CompleteAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ eCommerce.Infrastructure/UnitOfWorks/RepositoryManager.cs
@@ -0,0 +1,42 @@
+﻿using eCommerce.Domain.Entities;
+using eCommerce.Domain.Interfaces;
+using eCommerce.Domain.Interfaces.Cart;
+using eCommerce.Domain.Interfaces.UnitOfWork;
+using eCommerce.Infrastructure.Data;
+using eCommerce.Infrastructure.Repositories;
+using eCommerce.Infrastructure.Repositories.Cart;
+using eCommerce.Infrastructure.Repositories.FavouriteSpecific;
+namespace eCommerce.Infrastructure.UnitOfWorks
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly AppDbContext dbContext;
+        private readonly  Lazy<IProductRepository>? productRepository;
+        private readonly Lazy<ICategory>? categoryRepository;
+        private readonly Lazy<IFavourite>? favouriteRepository;
+        private readonly Lazy<ICart>? cartRepository;
+        private readonly Lazy<IPaymentMethod>? paymentMethodRepository;
+        public RepositoryManager(AppDbContext context)
+        {
+            dbContext = context;
+            productRepository = new Lazy<IProductRepository>(() => new ProductRepository(context));
+            categoryRepository = new Lazy<ICategory>(() => new CategoryRepository(context));
+            favouriteRepository = new Lazy<IFavourite>(() => new FavouriteRepository(context));
+            cartRepository = new Lazy<ICart>(() => new CartRepository(context));
+            paymentMethodRepository = new Lazy<IPaymentMethod>(() => new PaymentMethodRepository(context));
+        }
+
+        public IProductRepository Product => productRepository!.Value;
+
+        public ICategory Categorie => categoryRepository!.Value;
+
+        public IFavourite Favorite => favouriteRepository!.Value;
+
+        public ICart Cart => cartRepository!.Value;
+
+        public IPaymentMethod PaymentMethod => paymentMethodRepository!.Value;
+
+        public async Task<int> CompleteAsync(CancellationToken cancellationToken) =>
+            await dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
